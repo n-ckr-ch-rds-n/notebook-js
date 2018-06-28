9ec97913e7d30e44aa24dd3e6b8697264090ea88
Nick <nicholasjohnrichardson@gmail.com>
2018-06-28 14:43:07 +0100

    view method on note list view updated. now only returns the first 20 characters of a note, wrapped in html

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,9 +1,9 @@
 (function(exports) {
   function testReturnsStringOfHTMLRepresentingNotelist() {
     var notelist = new Notelist();
-    notelist.store("house");
+    notelist.store("Hello my name is Nick and everything is hunky dory");
     var notelistview = new NotelistView(notelist);
-    assert.isTrue(notelistview.view() === "<ul><li><div>house</div></li></ul>")
+    assert.isTrue(notelistview.view() === "<ul><li><div>Hello my name is Nic</div></li></ul>")
   };
 
   testReturnsStringOfHTMLRepresentingNotelist();
